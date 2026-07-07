804fd14991de96e6e51a4651460a65e018ed1406
nnowak1103-sys <nnowak1103@gmail.com>
2026-07-07 18:36:22 -0400

    Delete SonicForgePro_NoLocalBuild directory

--- SonicForgePro_NoLocalBuild/Source/DSP/NoiseGate.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include <JuceHeader.h>
-namespace SonicForge { class NoiseGate { public: void prepare(double sr, int ch){ env.assign(ch,0.f); coeff=std::exp(-1.0/(0.01*sr)); } void process(juce::AudioBuffer<float>& b, float thresholdDb){ float th=juce::Decibels::decibelsToGain(thresholdDb); for(int c=0;c<b.getNumChannels();++c){ auto*d=b.getWritePointer(c); for(int i=0;i<b.getNumSamples();++i){ env[c]=std::max(std::abs(d[i]), env[c]*coeff); float g= env[c] < th ? env[c]/juce::jmax(th,1e-6f) : 1.f; d[i]*=juce::jlimit(0.f,1.f,g); }}} private: std::vector<float> env; float coeff=.99f; }; }
